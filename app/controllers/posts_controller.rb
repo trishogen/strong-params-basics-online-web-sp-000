10803f18c0141313bb08ff6758f09282a1b32e06
Trish Ogen <patriciaogen@gmail.com>
2019-10-31 01:43:28 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
 	def edit
 	  @post = Post.find(params[:id])
-	end 
+	end
 
 	private
 
